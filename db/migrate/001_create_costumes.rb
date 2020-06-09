ef4a30cd0018d22fbb205bb0eefbcbb79296c8bb
Parker Williamson <parker.williamson@gmail.com>
2020-06-09 17:44:26 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,7 +3,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
   def change 
     create_table :costumes do |t|
       t.string :name
-      t.float :price 
+      t.integer :price 
       t.string :size
       t.string :image_url
       t.timestamps
