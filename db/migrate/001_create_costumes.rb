82892694e294c887bca9ca1b57a671c3d39ccb4b
Parker Williamson <parker.williamson@gmail.com>
2020-06-09 17:45:30 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,12 +1,12 @@
-class CreateCostumes < ActiveRecord::Migration[5.2]
+# class CreateCostumes < ActiveRecord::Migration[5.2]
 
-  def change 
-    create_table :costumes do |t|
-      t.string :name
-      t.integer :price 
-      t.string :size
-      t.string :image_url
-      t.timestamps
-    end
-  end
-end
+#   def change 
+#     create_table :costumes do |t|
+#       t.string :name
+#       t.integer :price 
+#       t.string :size
+#       t.string :image_url
+#       t.timestamps
+#     end
+#   end
+# end
