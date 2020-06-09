8f98b582e0e0e36d60588d355baf4967ed340227
Parker Williamson <parker.williamson@gmail.com>
2020-06-09 18:12:28 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,6 +5,10 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :location
       t.string :theme
       t.integer :price 
-      
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date 
+      t.string :description
+    end
   end
 end
