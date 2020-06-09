f4fcaa65e7b2bd9f706a559ada259fb4645d90b7
Parker Williamson <parker.williamson@gmail.com>
2020-06-09 18:11:29 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,5 +1,10 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   def change
-    create_table :haunted_houses
+    create_table :haunted_houses do |t|
+      t.string :name 
+      t.string :location
+      t.string :theme
+      t.integer :price 
+      
   end
 end
